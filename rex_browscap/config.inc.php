8ff703910aa15efd0851ce19430c7144c97ff4ab
jdlx <jdlx@66f78373-6c1a-4988-8deb-0d2e18f365f1>
2010-10-19 00:02:34 +0000

    fix: slash zuviel in pfad zu cache datei..
    git-svn-id: svn://svn.rexdev.de/addons/rex_browscap/rex_browscap/trunk@20 66f78373-6c1a-4988-8deb-0d2e18f365f1

--- rex_browscap/config.inc.php
+++ rex_browscap/config.inc.php
@@ -3,7 +3,7 @@
 /**
  * RexBrowscap Addon
  * Based on http://code.google.com/p/phpbrowscap/
- * @author st DOT jonathan AT gmail DOT com 
+ * @author st DOT jonathan AT gmail DOT com
  * @author rexdev.de
  * @package redaxo4.2
  * @version svn:$Id$
@@ -15,13 +15,13 @@ $mypage = "rex_browscap";
 $REX['ADDON']['rxid'][$mypage] = '714';
 // NAME SHOWN IN THE REDAXO MAIN MENU
 $REX['ADDON']['name'][$mypage] = 'RexBrowscap';
- 
+
 $REX['ADDON']['version'][$mypage] = '0.1';
 $REX['ADDON']['author'][$mypage] = 'rexdev.de';
 $REX['ADDON']['supportpage'][$mypage] = 'forum.redaxo.de';
 
 // CACHE LOCATION
-$REX['ADDON']['rex_browscap']['cache'] = $REX['HTDOCS_PATH'].'/files/addons/rex_browscap/cache';
+$REX['ADDON']['rex_browscap']['cache'] = $REX['HTDOCS_PATH'].'files/addons/rex_browscap/cache';
 
 // --- DYN
 $REX['ADDON']['rex_browscap']['foo'] = 'fooval';
@@ -37,13 +37,13 @@ switch($phpversion)
   case ($phpversion<4):
     rex_warning('Mindestens PHP4 benötigt!');
     break;
-    
+
   case ($phpversion<5):
     // VERSION FÜR PHP 4
     rex_warning('Die PHP4 Version ist depreciated!');
     require_once('libs/php4/Browscap.php');
     break;
-    
+
   default:
     // VERSION FÜR PHP 5
     require_once('libs/php5/Browscap.php');
