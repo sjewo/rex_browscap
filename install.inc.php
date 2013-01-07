121506ad784c7717fff7be61569a29a1d60dcb02
jdlx <github@rexdev.de>
2013-01-07 13:44:01 +0100

    install folder check

--- install.inc.php
+++ install.inc.php
@@ -14,9 +14,26 @@
 // ADDON IDENTIFIER
 $mypage = "rex_browscap";
 
+
+// CHECK ADDON FOLDER NAME
+////////////////////////////////////////////////////////////////////////////////
+$addon_folder = basename(dirname(__FILE__));
+if($addon_folder != $myself)
+{
+  $REX['ADDON']['installmsg'][$addon_folder] = '<br />Der Name des Addon-Ordners ist inkorrekt: <code style="color:black;font-size:12px;">'.$addon_folder.'</code>
+                                                <br />Addon-Ordner in <code style="color:black;font-size:1.23em;">'.$mypage.'</code> umbenennen und Installation wiederholen';
+  $REX['ADDON']['install'][$addon_folder] = 0;
+  return;
+}
+
+
+// PHP VERSION CHECK
+////////////////////////////////////////////////////////////////////////////////
 if (intval(PHP_VERSION) < 5)
 {
-    $REX['ADDON']['installmsg'][$mypage] = 'Dieses Addon ben&ouml;tigt für volle Funktionalit&auml;t PHP 5! Die PHP 4 Version ist depreciated.';
+  $REX['ADDON']['installmsg'][$mypage] = 'Dieses Addon ben&ouml;tigt für volle Funktionalit&auml;t PHP 5! Die PHP 4 Version ist depreciated.';
+  $REX['ADDON']['install'][$mypage] = 0;
+  return;
 }
 
 $REX['ADDON']['install'][$mypage] = 1;
