0c2121c5711c098f609331f8707ee5b2bed05682
jdlx <github@rexdev.de>
2013-01-19 23:34:40 +0100

    add caching in SESSION..

--- functions/function.rex_get_browser.inc.php
+++ functions/function.rex_get_browser.inc.php
@@ -20,6 +20,14 @@ if (!function_exists('rex_get_browser'))
   {
     global $REX;
 
+    if(!session_id()){
+      session_start();
+    }
+
+    if(isset($_SESSION['rex_get_browser'])){
+      return $_SESSION['rex_get_browser'];
+    }
+
     $bc = new Browscap($REX['ADDON']['rex_browscap']['cache']);
     $bc->silent = $REX['ADDON']['rex_browscap']['silent'];
     $bc->userAgent = $REX['ADDON']['rex_browscap']['userAgent'];
@@ -29,7 +37,9 @@ if (!function_exists('rex_get_browser'))
     $bc->remoteIniUrl = 'http://tempdownloads.browserscap.com/stream.php?BrowsCapINI';
     $bc->remoteVerUrl = 'http://tempdownloads.browserscap.com/versions/version-date.php';
 
-    return $bc->getBrowser($user_agent, $return_array);
+    $_SESSION['rex_get_browser'] = $bc->getBrowser($user_agent, $return_array);
+
+    return $_SESSION['rex_get_browser'];
   }
 }
 
